877e93c81d3ae118354b79d188c602b727b9948b
Sophie King <102487877+theskywillbeblue@users.noreply.github.com>
2025-04-03 17:10:39 +0100

    prepare site for deploy - brush up css

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,8 @@
-import logo from '../src/assets/Logo.png'
-import user from '../src/assets/user.jpg'
+import logo from '../src/assets/Logo.svg'
+import user from '../src/assets/user.png'
 import {Link} from 'react-router'
 
 export default function Header() {
 
-	return (<header><Link to="/"><img src={logo} className='logo'/></Link><img src={user} className='user-logo'/></header>)
+	return (<header><Link to="/"><img src={logo} className='logo'/></Link><hr /><img src={user} className='user-logo'/></header>)
 }
